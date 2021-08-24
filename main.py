29ee71393e2dd585134c7b20a6a14e6bf7d6b26c
kikigigi <momokocute1@gmail.com>
2021-08-24 14:00:22 +0200

    "Update footer"

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
+from datetime import datetime
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -7,12 +8,14 @@ Bootstrap(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    current_year = datetime.now().year
+    return render_template('index.html',  current_year=current_year)
 
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    current_year = datetime.now().year
+    return render_template('dashboard.html', current_year=current_year)
 
 
 if __name__ == '__main__':
